31077228fccacc6d239ef06f3ed4d0a17e5a100b
DelisaRogger <delrog47231@stud.noroff.no>
2024-01-21 11:37:49 +0100

    specific grid fix

--- js/jacket-specific.js
+++ js/jacket-specific.js
@@ -1,4 +1,4 @@
-const detailContainer = document.querySelector(".specific-description");
+const detailContainer = document.querySelector(".jacket-specific-container");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 
@@ -25,10 +25,21 @@ fetchDetails();
 
 function createHtml(details) {
     detailContainer.innerHTML = `
-        <img class="product-img" src="${details.image}" alt="${details.title}" />
+        <img class="specific-img" src="${details.image}" alt="${details.title}" />
+      <div class="specific-description">
         <h1>${details.title}</h1>
         <p>${details.description}</p>
         <p class="price-description">$${details.price}</p>
-        <a href="check_out.html" class="cta cta-cart">Add to cart</a>
+        <label for="size-select">Select Size:</label>
+            <select id="size-select">${createSizeOptions()}
+            </select>
+      </div>        
+      <a href="check_out.html" class="cta cta-cart">Add to cart</a>
     `;
 }
+
+function createSizeOptions() {
+  const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+  return sizes.map((size, index) => `<option value="${index}">${size}</option>`).join("");
+}
+
