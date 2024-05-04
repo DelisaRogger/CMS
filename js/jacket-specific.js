4714bfb287c32e5f2e162617a635726f70b02465
DelisaRogger <delrog47231@stud.noroff.no>
2024-05-04 13:51:23 +0200

    3rd

--- js/jacket-specific.js
+++ js/jacket-specific.js
@@ -4,7 +4,7 @@ const params = new URLSearchParams(queryString);
 
 let id = params.get("id");
 
-const detailUrl = `https://api.noroff.dev/api/v1/rainy-days/${id}`;
+const detailUrl = `https://cors.noroff.dev/http://rainydelrog.no/rainydays/wp-json/wc/store/products/${id}`;
 const loader = document.getElementById("loader");
 
 async function fetchDetails() {
@@ -14,12 +14,12 @@ async function fetchDetails() {
         const response = await fetch(detailUrl);
         const details = await response.json();
 
-        document.title = details.title;
+        document.title = details.name;
 
         createHtml(details);
 
     } catch (error) {
-        detailContainer.innerHTML = `<p>Opps! There seems to be an error loading the product details. Try again later.: ${error.message}</p>`;
+        detailContainer.innerHTML = `<p>Oops! There seems to be an error loading the product details. Try again later.: ${error.message}</p>`;
         
     } finally {
       loader.style.display = "none";
@@ -32,16 +32,16 @@ fetchDetails();
 
 function createHtml(details) {
     detailContainer.innerHTML = `
-        <img class="specific-img" src="${details.image}" alt="${details.title}" />
+      <img class="product-img" src="${details.images[0].src}" alt="${details.name}" />
       <div class="specific-description">
-        <h1>${details.title}</h1>
+        <h1>${details.name}</h1>
         <p>${details.description}</p>
         <p class="price-description">$${details.price}</p>
         <label for="size-select">Select Size:</label>
             <select id="size-select">${createSizeOptions()}
             </select>
         <label for="gender">Gender</label>
-            <select id="gender">${details.gender}
+            <select id="gender">
             <option value="empty">-</option>
             <option value="male">M</option>
             <option value="female">F</option>
@@ -50,10 +50,3 @@ function createHtml(details) {
       <a href="check_out.html" class="cta cta-cart">Add to cart</a>
     `;
 }
-
-function createSizeOptions() {
-  const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
-  return sizes.map((size, index) => `<option value="${index}">${size}</option>`).join("");
-}
-
-
